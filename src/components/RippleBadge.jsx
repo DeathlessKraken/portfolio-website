f009d4b6dba1167d82f5b34130d72132b789fc89
Sean <michaelbrown.sean@gmail.com>
2024-03-27 10:45:42 -0700

    ripple badge redesign

--- src/components/RippleBadge.jsx
+++ src/components/RippleBadge.jsx
@@ -4,9 +4,11 @@ import styles from './ripplebadge.module.css';
 export default function RippleBadge() {
     return (
         <div className={styles.rippleBadge}>
-            <p>Looking for work...</p>
-            <div className={styles.ripple}>
-                <i className="fa-solid fa-magnifying-glass"></i>
+            <div className={styles.rippleBadgeBorder}>
+                <p>Looking for work...</p>
+                <div className={styles.ripple}>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
             </div>
         </div>
     );
