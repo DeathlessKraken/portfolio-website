35ec685f255d676f09a6ddf353137c0f874b9cc1
Sean <michaelbrown.sean@gmail.com>
2024-03-26 18:36:24 -0700

    changed hover and added text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,65 +1,6 @@
 import React from "react";
 import styles from './aboutme.module.css';
 
-/*
-<Stack 
-                divider={<Divider variant="middle" flexItem/>} 
-                height='100%'
-                justifyContent='space-around'
-                spacing={2}
-            >
-            <Box display='flex' flexDirection='row' justifyContent={isMobile ? 'space-between' : 'space-around'} alignItems='center'>
-                <Box sx={{ margin: '6px' }}>
-                    <Typography variant="h3" sx={{ lineHeight: 1 }}>
-                        Sean Brown
-                    </Typography>
-                    <Box className="darkerText">
-                        <Typography variant="p" color='primary.light'>
-                            Web Designer
-                        </Typography>
-                        <Typography color='primary.light'>
-                            Software Developer
-                        </Typography>
-                    </Box>
-                </Box>
-                <Grid container columns={2} spacing={2} sx={{ width: 'fit-content' }} direction={isMobile ? 'column' : 'row'} >
-                    <Grid item xs="auto" display="flex" alignItems="center" justifyContent="space-around">
-                        <Avatar 
-                            alt="Profile picture of a handsome dude"
-                            src="./images/pfp.jpeg"
-                            sx={{ width: 150, height: 150, boxShadow: 7 }}
-                        />
-                    </Grid>
-                    <Grid item xs="auto">
-                        <Stack 
-                            alignItems='center' 
-                            justifyContent="space-around" 
-                            height={1}
-                            direction={isMobile ? 'row' : 'column'}
-                        >
-                            <Link href="https://github.com/SBSoftwareDev">
-                                <Icon className="fa-brands fa-github lighterText" />
-                            </Link>
-                            <Link href="https://stackoverflow.com/users/3557315/">
-                                <Icon className="fa-brands fa-stack-overflow lighterText" />
-                            </Link>
-                            <Link href="https://www.linkedin.com/in/sean-brown-a98a512b3/">
-                                <Icon className="fa-brands fa-linkedin lighterText" />
-                            </Link>
-                        </Stack>
-                    </Grid>
-                </Grid>
-            </Box>
-            <Typography variant="p" color='#D3D3D3' sx={{margin: '6px', lineHeight: 1.5, fontSize: 18}} >
-                Hey there! I'm Sean, a computer scientist and software developer. 
-                I attended 
-                <Typography variant="p" color='#cfd82d' sx={{lineHeight: 1.5, fontSize: 18}}> Portland State University </Typography> 
-                where I developed a healthy foundation in CS. 
-                I spend my free time in the garage, on my SV650, or in the kitchen!
-            </Typography>
-            </Stack>
-*/
-
 function AboutMe() {
 
     return (
@@ -75,11 +16,27 @@ function AboutMe() {
                 </div>
             </div>
             <div className={styles.aboutContent}>
-                <p>Hey there! I'm Sean, a computer scientist and software developer. 
-                I attended <span>Portland State University</span> where I developed a healthy foundation in CS. 
-                I spend my free time in the garage, on my SV650, or in the kitchen!</p>
-
-                <p>Go on to talk about your CS career and professional life...</p>
+                <p>
+                    Hey there! I'm Sean, a computer scientist and software developer. 
+                    I attended <b>Portland State University</b> where I developed a healthy foundation in CS. 
+                    I spend my free time in the garage, on my SV650, or in the kitchen!
+                </p>
+                <p>
+                    My journey <i>really</i> started when I was a wee lad, messing around with Windows batch files, 
+                    but that's a story for another day.
+                </p>
+                <p>
+                    I was most recently employed as an Assistant Manager at Discount Tire in Tigard, Oregon.
+                    In December 2023, I set out to finish what I started years ago: my path to becoming a Full Stack
+                    Web Developer. Ever since then, building on my knowledge from <b>PSU</b>, I've 
+                    been <b>educating myself</b> full-time.
+                </p>
+                <p>
+                    While technology of any kind fascinates me, I've been repeatedly drawn to the discipline of 
+                    Computer Science. In my formative years, I taught myself to 
+                    make <b>chat clients, games, game engines, and mobile applications</b> &#40;I've also fantastic
+                    stories of handmade radios and a bench power supply&#41;.
+                </p>
             </div>
             <div className={styles.aboutNav}>
                 <a href="https://github.com/SBSoftwareDev">
