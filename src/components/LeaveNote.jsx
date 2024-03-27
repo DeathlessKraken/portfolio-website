dccfe7a42e17a3cbdeb309ec383573fff669fe99
Sean <michaelbrown.sean@gmail.com>
2024-03-27 14:27:35 -0700

    updated layout and style

--- src/components/LeaveNote.jsx
+++ src/components/LeaveNote.jsx
@@ -27,62 +27,81 @@ export default function Sandbox() {
             </div>
 
             <div className={styles.noteInputs}>
-                <div>
-                    <label htmlFor="name">Name: </label>
-                    <input 
-                        type="text" 
-                        name="name"
-                        id="name"
+                <div className={styles.smallInputContainer}>
+                    <div className={styles.smallInput}>
+                        <div>
+                            <label htmlFor="name">Name: </label>
+                            <p>*</p>
+                        </div>
+                        <input 
+                            type="text" 
+                            name="name"
+                            id="name"
+                            required
+                            value={input.name}
+                            onChange={handleChange}
+                            maxLength={31}
+                            size={20}
+                            className={styles.textArea}
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div className={styles.smallInput}>
+                        <div>
+                            <label htmlFor="email">Contact E-mail: </label>
+                            <p>*</p>
+                        </div>
+                        <input 
+                            type="email" 
+                            name="email"
+                            id="email"
+                            required
+                            value={input.email}
+                            onChange={handleChange}
+                            maxLength={31}
+                            size={20}
+                            className={styles.textArea}
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div className={styles.smallInput}>
+                        <div>
+                            <label htmlFor="phone">Phone Number: </label>
+                            <p></p>
+                        </div>
+                        <input 
+                            type="tel" 
+                            name="phone"
+                            id="phone"
+                            value={input.phone}
+                            onChange={handleChange}
+                            maxLength={20}
+                            size={20}
+                            className={styles.textArea}
+                            autoComplete="off"
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.largeInputContainer}>
+                    <div>
+                        <label htmlFor="note">Note: </label>
+                        <p>*</p>
+                    </div>
+                    <textarea 
+                        name="note"
+                        id="note"
                         required
-                        value={input.name}
+                        value={input.note}
                         onChange={handleChange}
-                        maxLength={31}
-                        size={20}
+                        placeholder="Leave a message..."
+                        maxLength={999}
+                        rows={7}
+                        cols={60}
+                        className={styles.textArea}
                     />
                 </div>
-
-                <div>
-                    <label htmlFor="email">Contact E-mail: </label>
-                    <input 
-                        type="email" 
-                        name="email"
-                        id="email"
-                        required
-                        value={input.email}
-                        onChange={handleChange}
-                        maxLength={31}
-                        size={20}
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="phone">Phone Number: </label>
-                    <input 
-                        type="tel" 
-                        name="phone"
-                        id="phone"
-                        value={input.phone}
-                        onChange={handleChange}
-                        maxLength={20}
-                        size={20}
-                    />
-                </div>
-
             </div>
-
-            <label htmlFor="note">Note: </label>
-            <textarea 
-                name="note"
-                id="note"
-                required
-                value={input.note}
-                onChange={handleChange}
-                placeholder="Leave a message..."
-                maxLength={999}
-                rows={7}
-                cols={60}
-                className={styles.noteField}
-            />
         </div>
     );
 }
