bff57b925fb007c6dab95547fa164148c813e100
Sean <michaelbrown.sean@gmail.com>
2024-03-27 16:43:05 -0700

    added work experience

---
+++ src/components/Work.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from './work.module.css';
+
+export default function Work() {
+    return (
+        <div className={styles.work}>
+            <h1>Professional Experience</h1>
+            <hr />
+            <div className={styles.careerContainer}>
+                <div className={styles.career}>
+                    <h3 className={styles.gridJob}>Discount Tire</h3>
+                    <h3 className={styles.gridLocation}>Tigard, Oregon</h3>
+                    <h3 className={styles.gridPosition}>Assistant Manager</h3>
+                    <h3 className={styles.gridTime}>2022-2024</h3>
+                    <ul className={styles.gridDesc}>
+                        <li>
+                            Personally <span>managed key store operations</span> such as 
+                            outbound product, store inventory, machine maintenance,
+                            and Technician training.
+                        </li>
+                        <li>
+                            Consistently <span>top-ranked in unit sales</span> value. I develop personal
+                            relationships with people, identify their needs and what 
+                            may work best for them. 
+                        </li>
+                        <li>
+                            Provided mentorship to new employees, <span>communicating clear and 
+                            concise directions</span> for optimal safety.
+                        </li>
+                        <li>
+                            Maintained <span>accountablity with daily audits</span> that provide insight and seed 
+                            growth for future oppotunities
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
