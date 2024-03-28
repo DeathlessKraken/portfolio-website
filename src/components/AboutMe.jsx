075d76bb7104ff368d29384a1eab1963f9682e77
Sean <michaelbrown.sean@gmail.com>
2024-03-28 16:49:37 -0700

    linting edit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import RippleBadge from "./RippleBadge";
 import LeaveNote from './LeaveNote';
 import styles from './aboutme.module.css';
@@ -22,24 +21,24 @@ function AboutMe() {
             </div>
             <div className={styles.aboutContent}>
                 <p>
-                    Hey there! I'm Sean, a computer scientist and software developer. 
+                    Hey there! I&apos;m Sean, a computer scientist and software developer. 
                     I attended <b>Portland State University</b> where I developed a healthy foundation in Computer Science. 
                     I spend my free time in the garage, on my SV650, or in the kitchen!
                 </p>
                 <p>
                     My journey <i>really</i> started when I was a wee lad, messing around with Windows batch files, 
-                    but that's a story for another day.
+                    but that&apos;s a story for another day.
                 </p>
                 <p>
                     I was most recently employed as an Assistant Manager at Discount Tire in Tigard, Oregon.
                     In December 2023, I set out to finish what I started at university: my path to becoming a Full Stack
-                    Web Developer. Ever since then, building on my knowledge from <b>PSU</b>, I've 
+                    Web Developer. Ever since then, building on my knowledge from <b>PSU</b>, I&apos;ve 
                     been <b>educating myself</b> full-time.
                 </p>
                 <p>
-                    While technology of any kind fascinates me, I've been repeatedly drawn to the discipline of 
+                    While technology of any kind fascinates me, I&apos;ve been repeatedly drawn to the discipline of 
                     Computer Science. In my formative years, I taught myself to 
-                    make <b>chat clients, games, game engines, and mobile applications</b> &#40;I've also fantastic
+                    make <b>chat clients, games, game engines, and mobile applications</b> &#40;I&apos;ve also fantastic
                     stories of handmade radios and a bench power supply&#41;.
                 </p>
             </div>
--- src/components/ExperienceContent.jsx
+++ src/components/ExperienceContent.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Project from "./projects/Project";
 import styles from './experiencecontent.module.css';
 import Work from "./Work";
