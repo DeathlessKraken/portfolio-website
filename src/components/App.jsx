3d251f3a1b46a0fd02c89c0ca72fa2da73e2b484
Sean <michaelbrown.sean@gmail.com>
2024-03-29 15:43:00 -0700

    non-functional mobile appbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import AboutMe from "./AboutMe";
 import ExperienceContent from './ExperienceContent';
 import Footer from "./Footer";
 import Profile from "./Profile";
-import NavBar from "./NavBar";
+import NavBarDesktop from "./NavBarDesktop";
+import NavBarMobile from "./NavBarMobile";
+import useMediaQuery from './functions/useMediaQuery';
 import styles from "./app.module.css";
 
 export default function App(){
+    const isMobile = useMediaQuery('(max-width: 840px)');
+
     return (
         <div className={styles.appContainer}>
             <div className={styles.app}> 
-                <NavBar />
+                {isMobile ? <NavBarMobile /> : <NavBarDesktop />}
                 <Profile />
                 <AboutMe />
                 <ExperienceContent />
