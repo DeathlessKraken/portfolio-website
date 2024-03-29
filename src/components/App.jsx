052a24d00a493a577acc3a44e465c8d88d9fa991
Sean <michaelbrown.sean@gmail.com>
2024-03-29 15:13:06 -0700

    initial desktop navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import AboutMe from "./AboutMe";
 import ExperienceContent from './ExperienceContent';
 import Footer from "./Footer";
 import Profile from "./Profile";
+import NavBar from "./NavBar";
 import styles from "./app.module.css";
 
 export default function App(){
     return (
         <div className={styles.appContainer}>
             <div className={styles.app}> 
+                <NavBar />
                 <Profile />
                 <AboutMe />
                 <ExperienceContent />
